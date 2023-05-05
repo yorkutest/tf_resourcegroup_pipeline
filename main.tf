822680ddf58cf33a5c8f871b8c430a0843149e96
automagicallyio <max@automagically.io>
2023-05-04 22:53:12 -0400

    tf_resourcegroup_module

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "rg" {
-  source = "git::https://github.com/yorkutest/tf_resourcegroup_pipeline?ref=maxtest"
+  source = "git::https://github.com/yorkutest/tf_resourcegroup_module?ref=main"
   resourceGroupName     = var.resourceGroupName
   resourceGroupLocation = var.resourceGroupLocation
   tags                  = var.tags
