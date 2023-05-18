41524b8d206b647399a3e6e3734a08d0c6d54eda
Vineet Sharma <vineet2004@gmail.com>
2023-05-17 23:33:49 -0400

    fixing thngs up

--- providers.tf
+++ providers.tf
@@ -3,29 +3,18 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.55.0"
+      version = "3.56.0"
     }
   }
   backend "azurerm" {
+    use_oidc = true
   }
 }
-
-#provider "azurerm" {
-#  features {}
-#  skip_provider_registration = true
-#  use_msi                    = true
-#  subscription_id            = var.subscriptionId
-#}
 
 provider "azurerm" {
   features {}
   skip_provider_registration = true
-  subscription_id            = var.subscriptionId
   use_oidc                   = true
-
-  # for GitHub Actions
-  oidc_request_token = var.oidc_request_token
-  oidc_request_url   = var.oidc_request_url
-
+  subscription_id            = var.subscriptionId
 }
 
--- variables.tf
+++ variables.tf
@@ -1,19 +1,8 @@
-###### Required variables for the provider  ############
-variable "oidc_request_token" {
+variable "subscriptionId" {
   type = string
 }
-variable "oidc_request_url" {
-  type = string
-}
-
-###### End: Required variables for the provider  ########
-
-
 
 variable "resourceGroupName" {
-  type = string
-}
-variable "subscriptionId" {
   type = string
 }
 
