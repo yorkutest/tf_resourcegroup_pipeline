11b0c2e8bf99bf3d86770bb11549289486ae3df5
Vineet Sharma <vsharma@objectsharp.com>
2023-06-01 12:30:55 -0400

    adding a new version of module

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "rg" {
-  source                = "git::https://github.com/yorkutest/tf_resourcegroup_module?ref=v0.1"
+  source                = "git::https://github.com/yorkutest/tf_resourcegroup_module?ref=release/v0.2"
   resourceGroupName     = var.resourceGroupName
   resourceGroupLocation = var.resourceGroupLocation
   tags                  = var.tags
